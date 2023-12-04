f7c68cc3ddd3ec771d84dc171577d879c56ab15a
asafcastigliano <asafcastigliano@gmail.com>
2023-12-04 16:05:34 +0100

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,32 @@
+team = [
+    {
+        name: "Wayne Barnett",
+        role: "Fouder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Agela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manger",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+]
